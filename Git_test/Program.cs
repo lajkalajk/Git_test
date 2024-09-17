c45458face48cdf8e86ff618981cda51bc487768
jan.lajka <jan.lajka@student.gymspit.cz>
2024-09-17 08:45:44 +0200

    hi

--- Git_test/Program.cs
+++ Git_test/Program.cs
@@ -2,7 +2,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //hi﻿
+        //hi﻿ hi
         Console.WriteLine("ahoi");
         Console.WriteLine("aho");
 
