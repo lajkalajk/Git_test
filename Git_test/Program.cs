8c183514a3e32c29ce7f16e501e936c63c1c73e0
jan.lajka <jan.lajka@student.gymspit.cz>
2024-09-17 08:38:08 +0200

    change multiply

--- Git_test/Program.cs
+++ Git_test/Program.cs
@@ -8,6 +8,7 @@ internal class Program
 
         Console.WriteLine("2 + 2 = " + Add(2, 2));
         Console.WriteLine("2 - 2 = " + Subtract(2, 2));
+        Console.WriteLine("2 * 2 = " + Multiply(2, 2));
     }
 
     public static int Add(int x, int y)
@@ -17,6 +18,10 @@ internal class Program
     public static int Subtract(int x, int y)
     {
         return x - y;
+    }
+    public static int Multiply(int x, int y)
+    {
+        return x * y;
     }
 }
 
