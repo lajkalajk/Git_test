741e11f9ecdb40cb9c0ba261151f1e2be2fac735
jan.lajka <jan.lajka@student.gymspit.cz>
2024-09-17 08:27:38 +0200

    Change subtract

--- Git_test/Program.cs
+++ Git_test/Program.cs
@@ -7,11 +7,16 @@ internal class Program
         Console.WriteLine("aho");
 
         Console.WriteLine("2 + 2 = " + Add(2, 2));
+        Console.WriteLine("2 - 2 = " + Add(2, 2));
     }
 
     public static int Add(int x, int y)
     {
         return x + y;
+    }
+    public static int Subtract(int x, int y)
+    {
+        return x - y;
     }
 }
 
